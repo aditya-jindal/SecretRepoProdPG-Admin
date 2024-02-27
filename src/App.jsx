0ed162d11ab8a3a2b3404276604b34f1178841ba
aditya-jindal <139961475+aditya-jindal@users.noreply.github.com>
2024-02-27 23:05:58 +0530

    updated heading to mathElite

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
   return (
     <div>
       <div className="header">
-        <h1>MathFest 2023 Scores Table</h1>
+        <h1>MathElite 2024 Scores Table</h1>
         <button onClick={handleClick}>Refresh</button>
         <button onClick={handleGenerate}>Export to Excel</button>
       </div>
